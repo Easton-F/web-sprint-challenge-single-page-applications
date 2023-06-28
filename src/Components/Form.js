c3588ae25109607af335086ebc3e759f820794b4
Easton <easton.fritsch21@gmail.com>
2023-06-28 10:03:51 -0600

    added id's to name and size labels, Tests 5 and 7 passing. Still working on 6.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+    name: yup.string().required('name must be at least 2 characters!'),
+    size: yup.string().oneOf(['1', '2', '3'], 'must select a size!')
+})
 
 const Form = (props) => {
+    const [form, setForm] = useState({ name: '', size: ''})
 
     return (
         <div>
             <form id="pizza-form">
                 <h1>Build Your Own Pizza!</h1>
-                <label>Choice of Size
-                    <select>
-                        <option>--Select One--</option>
-                        <option>Small</option>
-                        <option>Medium</option>
-                        <option>Large</option>
+                <label id="name-input">What is your name?
+                    <input type="text" value={form.name}/>
+                </label>
+                <label id="size-dropdown">Choice of Size
+                    <select value={form.size}>
+                        <option value="">--Select One--</option>
+                        <option value="1">Small</option>
+                        <option value="2">Medium</option>
+                        <option value="3">Large</option>
                     </select>
                 </label>
                 <label>Choice of Sauce
