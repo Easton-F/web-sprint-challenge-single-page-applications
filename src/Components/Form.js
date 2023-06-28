e6b04a9e5900492b6baa045104548ac1589629b6
Easton <easton.fritsch21@gmail.com>
2023-06-28 09:44:22 -0600

    created a choice of size label on the form and got the form to render. Test 3 passing

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,9 +4,22 @@ const Form = (props) => {
 
     return (
         <div>
-            <h1>This is where you make pizza!</h1>
+            <form>
+                <h1>Build Your Own Pizza!</h1>
+                <label>Choice of Size
+                    <select>
+                        <option>--Select One--</option>
+                        <option>Small</option>
+                        <option>Medium</option>
+                        <option>Large</option>
+                    </select>
+                </label>
+                <label>Choice of Sauce
+                    
+                </label>
+            </form>
         </div>
     )
 }
 
-export default Form();
+export default Form;
