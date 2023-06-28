2cda44e99e3d4d8e7e990ac656727d298529d291
Easton <easton.fritsch21@gmail.com>
2023-06-28 09:30:43 -0600

    added a components folder and a Form.js. Test 2 is passing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Form from './Components/Form';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+          <Link to="/">Home Page</Link>&nbsp;
+          <Link to="pizza" id="order-pizza">DIY Pizza</Link>
+      </nav>
+      <Routes>
+          <Route path="pizza" element={<Form />}/>
+      </Routes>
     </>
   );
 };
---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Form = (props) => {
+
+    return (
+        <div>
+            <h1>This is where you make pizza!</h1>
+        </div>
+    )
+}
+
+export default Form();
