24f9022e12578144a025cbbf81b00e23723d504a
Easton <easton.fritsch21@gmail.com>
2023-06-28 10:44:14 -0600

    added a toppings checklist as well as a special instructions box. Added schema using yup but still have to implement it. Also toppings comes from the topping.js component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import * as yup from 'yup';
+import { toppings } from "./toppings";
 
 const schema = yup.object().shape({
     name: yup.string().required('name must be at least 2 characters!'),
@@ -7,25 +8,45 @@ const schema = yup.object().shape({
 })
 
 const Form = (props) => {
-    const [form, setForm] = useState({ name: '', size: ''})
+    const [form, setForm] = useState({ name: '', size: '', topping: '', special: ''})
+
+    const change = event => {
+        const { value, type, checked, name } = event.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        setForm({...form, [name]: valueToUse})
+    }
 
     return (
         <div>
             <form id="pizza-form">
                 <h1>Build Your Own Pizza!</h1>
                 <label id="name-input">What is your name?
-                    <input type="text" value={form.name}/>
+                    <input onChange={change} type="text" value={form.name} name="name"/>
                 </label>
                 <label id="size-dropdown">Choice of Size
-                    <select value={form.size}>
+                    <select onChange={change} value={form.size} name="size">
                         <option value="">--Select One--</option>
                         <option value="1">Small</option>
                         <option value="2">Medium</option>
                         <option value="3">Large</option>
                     </select>
                 </label>
-                <label>Choice of Sauce
-                    
+                <div>Add Toppings
+                {toppings.map(({ name }) => {
+                    return (
+                        <div>
+                            <input 
+                                type="checkbox"
+                                name={name}
+                                value={name}
+                            />
+                            <label>{name}</label>
+                        </div>
+                    );
+                })}
+                </div>
+                <label id="special-text">Special Instructions
+                    <input onChange={change} type="text" value={form.special} name="special"/>
                 </label>
             </form>
         </div>
---
+++ src/Components/toppings.js
@@ -0,0 +1,20 @@
+export const toppings = [
+    {
+        name: "Pepporoni"
+    },
+    {
+        name: "Olives"
+    },
+    {
+        name: "Bacon"
+    },
+    {
+        name: "Sausage"
+    },
+    {
+        name: "Ham"
+    },
+    {
+        name: "Pineapple"
+    }
+];
